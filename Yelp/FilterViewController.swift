2aeda94d4003ae30e1235f2b3981f0448887b90f
ducminh1012 <ducminh1012@gmail.com>
2016-10-25 00:02:24 +0700

    Fix collapse table

--- Yelp/FilterViewController.swift
+++ Yelp/FilterViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class FilterViewController: UIViewController {
 
-    var isFiltered = false
+    var isFiltered = ["Distance": false, "Sort": false, "Category": false]
     
     var isExpand = ["Distance": false, "Sort": false, "Category": false]
     var filterData = ["Distance": ["0.3", "1", "2", "3", "4", "5"],
                           "Sort": ["Best match"],
                           "Category": ["Viet", "Thai"]]
-    var selectedFilter = [String: String]()
+    var selectedFilter = [String: (IndexPath, String)]()
     
     @IBOutlet weak var filterTableView: UITableView!
     @IBAction func onCancelButton(_ sender: UIBarButtonItem) {
@@ -31,7 +31,9 @@ class FilterViewController: UIViewController {
 
         filterTableView.delegate = self
         filterTableView.dataSource = self
-
+        
+        selectedFilter = ["Distance": (IndexPath(row: 0, section: 0),""), "Sort": (IndexPath(row: 0, section: 1),""), "Category": (IndexPath(row: 0, section: 2),"")]
+        
     }
 
 }
@@ -74,28 +76,61 @@ extension FilterViewController: UITableViewDelegate, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
         switch indexPath.section {
         case 0:
             let filterCell = tableView.dequeueReusableCell(withIdentifier: "filterCell") as! FilterCell
-            
+            filterCell.selectedBackgroundView = UIView()
             return filterCell
         // Distance
         case 1:
             let filterCell2 = tableView.dequeueReusableCell(withIdentifier: "filterCell2") as! FilterCell2
-            filterCell2.distanceLabel.text = isFiltered ? selectedFilter["Distance"] : filterData["Distance"]![indexPath.row] + " miles"
+            
+            let index = selectedFilter["Distance"]?.0
+            
+            if indexPath.row ==  index?.row || (indexPath.row == 0 && isFiltered["Distance"]!){
+                filterCell2.checkButton.setImage(UIImage(named: "check"), for: .normal)
+            }else{
+                filterCell2.checkButton.setImage(UIImage(named: "uncheck"), for: .normal)
+            }
+            
+            
+            filterCell2.distanceLabel.text = isFiltered["Distance"]! ? selectedFilter["Distance"]!.1 + " miles": filterData["Distance"]![indexPath.row] + " miles"
+            filterCell2.selectedBackgroundView = UIView()
             return filterCell2
         // Sort
         case 2:
             let filterCell2 = tableView.dequeueReusableCell(withIdentifier: "filterCell2") as! FilterCell2
-            filterCell2.distanceLabel.text = isFiltered ? selectedFilter["Sort"] : filterData["Sort"]?[indexPath.row]
+            
+            let index = selectedFilter["Sort"]?.0
+            
+            if indexPath.row ==  index?.row || (indexPath.row == 0 && isFiltered["Sort"]!){
+                filterCell2.checkButton.setImage(UIImage(named: "check"), for: .normal)
+            }else{
+                filterCell2.checkButton.setImage(UIImage(named: "uncheck"), for: .normal)
+            }
+            
+            filterCell2.distanceLabel.text = isFiltered["Sort"]! ? selectedFilter["Sort"]?.1 : filterData["Sort"]?[indexPath.row]
+            filterCell2.selectedBackgroundView = UIView()
             return filterCell2
         // Category
         case 3:
             let filterCell2 = tableView.dequeueReusableCell(withIdentifier: "filterCell2") as! FilterCell2
-            filterCell2.distanceLabel.text = isFiltered ? selectedFilter["Category"] : filterData["Category"]?[indexPath.row]
+            
+            let index = selectedFilter["Category"]?.0
+            if indexPath.row ==  index?.row || (indexPath.row == 0 && isFiltered["Category"]!){
+                filterCell2.checkButton.setImage(UIImage(named: "check"), for: .normal)
+            }else{
+                filterCell2.checkButton.setImage(UIImage(named: "uncheck"), for: .normal)
+            }
+            
+            filterCell2.distanceLabel.text = isFiltered["Category"]! ? selectedFilter["Category"]?.1 : filterData["Category"]?[indexPath.row]
+            filterCell2.selectedBackgroundView = UIView()
             return filterCell2
         default:
             let filterCell2 = tableView.dequeueReusableCell(withIdentifier: "filterCell2") as! FilterCell2
+            filterCell2.selectedBackgroundView = UIView()
             return filterCell2
         }
         
@@ -134,15 +169,36 @@ extension FilterViewController: UITableViewDelegate, UITableViewDataSource{
         switch indexPath.section {
         case 1:
             
-            selectedFilter["Distance"] = filterData["Distance"]?[indexPath.row]
+            if !isExpand["Distance"]! {
+                isFiltered["Distance"] = false
+                
+            }else{
+                selectedFilter["Distance"]?.0 = indexPath
+                selectedFilter["Distance"]?.1 = filterData["Distance"]![indexPath.row]
+                isFiltered["Distance"] = true
+            }
             isExpand["Distance"] = !isExpand["Distance"]!
+            
         case 2:
             
-            selectedFilter["Sort"] = filterData["Sort"]?[indexPath.row]
+            if !isExpand["Sort"]! {
+                isFiltered["Sort"] = false
+                
+            }else{
+                selectedFilter["Sort"]?.0 = indexPath
+                selectedFilter["Sort"]?.1 = filterData["Sort"]![indexPath.row]
+                isFiltered["Sort"] = true
+            }
             isExpand["Sort"] = !isExpand["Sort"]!
         case 3:
-            
-            selectedFilter["Category"] = filterData["Category"]?[indexPath.row]
+            if !isExpand["Category"]! {
+                isFiltered["Category"] = false
+                
+            }else{
+                selectedFilter["Category"]?.0 = indexPath
+                selectedFilter["Category"]?.1 = filterData["Category"]![indexPath.row]
+                isFiltered["Category"] = true
+            }
             isExpand["Category"] = !isExpand["Category"]!
         default:
             break
